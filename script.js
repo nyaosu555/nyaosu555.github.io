c7bf22819a9af40036f2ee080196722b3d80fbe9
nyaosu555 <oneichan555@gmail.com>
2021-08-31 11:48:08 +0900

    スムーススクロール対応で支払方法の部分のhtml,css,script変更

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 {
 
 
+
 // ハンバーガーメニュー処理
 const spMenuIcon = document.getElementById('spMenuIcon');
 
@@ -15,12 +16,6 @@ spMenuIcon.addEventListener('click', function() {
 // トップへ戻る機能
 
   const menuLists = document.querySelectorAll('.home header .headerBottomContainer > nav ul li a');
-  
-  // menuLists.forEach(function(menuList) {
-  //   menuList.addEventListener('click', function(event) {
-  //     smoothScroll(event);
-  //   });
-  // });
 
   document.getElementById('topBackButton').addEventListener('click', function(event) {
     smoothScroll(event);
@@ -28,15 +23,18 @@ spMenuIcon.addEventListener('click', function() {
   
 // 各メニューリストをクリックした時にメニューを閉じる処理
 // forEachを使った場合
-menuLists.forEach(function(menulist) {
-  menulist.addEventListener('click', function(event) {
-    document.querySelector('header .headerBottomContainer').classList.remove('active');
-    document.getElementById('spMenuIcon').classList.remove('active');
-    document.querySelector('#mask').classList.remove('active');
+window.addEventListener('load', function(event) {
 
-    if(document.querySelector('body').classList.contains('home')) {
-      smoothScroll(event);
-    } 
+  menuLists.forEach(function(menulist) {
+    menulist.addEventListener('click', function(event) {
+      document.querySelector('header .headerBottomContainer').classList.remove('active');
+      document.getElementById('spMenuIcon').classList.remove('active');
+      document.querySelector('#mask').classList.remove('active');
+      
+      if(document.querySelector('body').classList.contains('home')) {
+        smoothScroll(event);
+      } 
+    });
   });
 });
 
@@ -88,67 +86,68 @@ let showTargetresults = [
     } else {
 
     
-    for (let i = 0; i < showTargetresults.length; i++) {
-      const showTargetElements = showTargetresults[i].getBoundingClientRect().top + showTargetresults[i].clientHeight * .2;
-      if(window.innerHeight > showTargetElements) {
-        showTargetresults[i].classList.add('showTarget');
+      for (let i = 0; i < showTargetresults.length; i++) {
+        const showTargetElements = showTargetresults[i].getBoundingClientRect().top + showTargetresults[i].clientHeight * .2;
+        if(window.innerHeight > showTargetElements) {
+          showTargetresults[i].classList.add('showTarget');
+        }
+      }
+      for (let i = 0; i < targetItemLiElements.length; i++) {
+        targetItemLiElements[i].style.animationDelay = '' + (i * .5) + 's';
+        const showTargetElements = targetItemLiElements[i].getBoundingClientRect().top + targetItemLiElements[i].clientHeight * .2;
+        if(window.innerHeight > showTargetElements) {
+          targetItemLiElements[i].classList.add('showTarget');
+        }
+      }
+      
+      // for (let i = 0; i < targetPayLiElements.length; i++) {
+      //   targetPayLiElements[i].style.animationDelay = '' + (i * .5) + 's';
+      //   const showTargetElements = targetPayLiElements[i].getBoundingClientRect().top + targetPayLiElements[i].clientHeight * .2;
+      //   if(window.innerHeight > showTargetElements) {
+      //     targetPayLiElements[i].classList.add('showTarget');
+      //   }
+      // }
+      
+      for (let i = 0; i < notices.length; i++) {
+        const showTargetElements = notices[i].getBoundingClientRect().top + notices[i].clientHeight * .2;
+        if(window.innerHeight > showTargetElements) {
+          notices[i].classList.add('showTarget');
+        }
       }
     }
-    for (let i = 0; i < targetItemLiElements.length; i++) {
-      targetItemLiElements[i].style.animationDelay = '' + (i * .5) + 's';
-      const showTargetElements = targetItemLiElements[i].getBoundingClientRect().top + targetItemLiElements[i].clientHeight * .2;
-      if(window.innerHeight > showTargetElements) {
-        targetItemLiElements[i].classList.add('showTarget');
-      }
-    }
-    
-    for (let i = 0; i < targetPayLiElements.length; i++) {
-      targetPayLiElements[i].style.animationDelay = '' + (i * .5) + 's';
-      const showTargetElements = targetPayLiElements[i].getBoundingClientRect().top + targetPayLiElements[i].clientHeight * .2;
-      if(window.innerHeight > showTargetElements) {
-        targetPayLiElements[i].classList.add('showTarget');
-      }
-    }
-    
-    for (let i = 0; i < notices.length; i++) {
-      const showTargetElements = notices[i].getBoundingClientRect().top + notices[i].clientHeight * .2;
-      if(window.innerHeight > showTargetElements) {
-        notices[i].classList.add('showTarget');
-      }
-    }
-  }
   });
-// }
 
 
+  function smoothScroll(event) {
 
-function smoothScroll(event) {
- 
-  event.preventDefault();
+      
+      event.preventDefault();
+      
+      let requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+      
+      let targetId;
 
-  let requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+      if(event.target.tagName === 'A') {
+        targetId = event.currentTarget.getAttribute('href');
+        if(targetId.includes('/')) {
+          targetId = targetId.replace('/', '');
+        }
+      }
+    
   
-  let targetId;
-  if(event.target.tagName === 'A') {
-    targetId = event.currentTarget.getAttribute('href');
-    if(targetId.includes('/')) {
-      targetId = targetId.replace('/', '');
-    }
-  } 
-  
-  if(event.target.tagName === 'IMG') {
-    targetId = 'header';
-  }
+      if(event.target.tagName === 'IMG') {
+        targetId = 'header';
+      }
   
   let duration = 1000;
-
+  
   let targetPosition;
-
+  
   // スクロール先の左上地点を取得 + ヘッダーの高さ
-
-  targetPosition = document.querySelector(targetId).offsetTop - 130;
-  // console.log(targetPosition)
-
+  
+  targetPosition = document.querySelector(targetId).offsetTop;
+  console.log(targetPosition)
+  
   // カレント位置（クリックした位置）
   const startPosition = window.pageYOffset;
   // 距離(=スクロールをする移動距離)
@@ -171,14 +170,14 @@ function smoothScroll(event) {
     window.scrollTo(0, easeInOut(progress, startPosition, distance, duration));
     if(progress < duration) requestAnimationFrame(step);
   }
-
+  
   function easeInOut(t, b, c, d) {
     t /= d/2;
     if (t < 1) return c/2*t*t + b;
     t--;
     return -c/2 * (t*(t-2) - 1) + b;
   };
-    
+  
 }
 
 }
